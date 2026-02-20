77b36832b24ef7d08fa8f16e32286cc1b610fa17
ekjot2345-collab <ekjot2345@gmail.com>
2026-02-20 23:10:37 +0530

    update

--- test/datatypes.cpp
+++ test/datatypes.cpp
@@ -13,3 +13,73 @@
 
 
                                    //                      Datatypes and variables                              //
+#include<iostream>
+using namespace std;
+ int main(){
+    /*
+int a =223;
+cout<<a << endl;
+
+char b='v';
+cout<<b<<endl;
+
+
+float r=3.14;
+cout<<r<<endl;
+
+double d=1.23;
+cout<<d<<endl;
+
+int size=sizeof(a);
+cout<<"size:"<<size<<endl;
+
+bool f=true;
+cout<<f<<endl;
+
+
+int a='a' ;
+cout<<a<<endl;
+
+char ch=99;
+cout<<ch<<endl;
+
+char ch1=123456;
+cout<<ch1<<endl;
+
+int a=2.6/1.3;//2
+cout<<a<<endl;
+
+int a=2.6/5;//0 beacuse you are trying to store flfoat inside int
+cout<<a<<endl;
+int a=2;
+int b=3;
+
+bool first=(a>=b);
+cout<<first<<endl;
+
+bool second=(a<=b);
+cout<<second<<endl;
+
+bool third=(a==b);
+cout<<third<<endl;
+
+bool fourth=(a!=b);
+cout<<fourth<<endl;
+
+bool fifth=(a==b);
+cout<<fifth<<endl;*/
+
+// int a=56;
+// cout<<!a<<endl;    //op //0
+
+int a=0;
+cout<<!a<<endl; //1
+
+
+
+    
+
+
+
+
+ }                   
