38a7686ef703e65a94a9ff591d2e430e29310c6c
ekjot2345-collab <ekjot2345@gmail.com>
2026-02-20 22:04:33 +0530

    update

---
+++ test/datatypes.cpp
@@ -0,0 +1,15 @@
+                                                     //Summary// 
+
+// #include<iostream>
+// #include<bits/stdc++.h>//includes all library
+
+// using namespace std;
+
+// int main(){
+//     // cout<<"namaste duniya"<< endl <<"hello bache";
+//     cout<<"namaste duniya"<< "\n" <<"hello bache";
+// }
+
+
+
+                                   //                      Datatypes and variables                              //
