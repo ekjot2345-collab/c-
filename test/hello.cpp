0ffcb0a90e0f8bc4ea33007409ab27d8a330446e
ekjot2345-collab <ekjot2345@gmail.com>
2026-02-20 15:38:40 +0530

    lets learn c++

---
+++ test/hello.cpp
@@ -0,0 +1,62 @@
+// #include<iostream>
+#include<bits/stdc++.h>//includes all library 
+
+using namespace std;
+
+int main(){
+    // std::cout<<"hello ekjot bete "<<std::endl;//<<"\n"; can also use \n and std::endl
+    // std::cout<<"hey striver!";
+
+
+
+
+
+
+    //int
+    // int x;
+    // cin>>x;
+    // cout<<"the value of x is: "<<x;
+
+    //long
+    // long x=150000;
+    // cout<<"the value of x is :"<<x;
+//int,long,longlong,float,double
+                                               //string and getline
+
+
+// string s1;
+// string s2;
+// cin>>s1>>s2;
+// cout<<s1<<"  "<<s2;
+
+
+// string s1;
+// cin>>s1;     
+// cout<<s1;
+////input :Hello ekjot           //output:Hello       (stop reading where space is there )
+
+
+// string str;
+// getline(cin,str);                                //it just picks up till the line breaks
+// cout<<str;
+
+//input :Hello ekjot           //output:Hello ekjot
+
+
+
+//char
+// char ch;
+// cin>>ch;
+// cout<<ch;
+
+
+
+//int,long,longlong,float,double
+// string and getline
+//char
+   
+
+return 0;
+
+}
+ 
