5922815f9ad37f8fcddf30d908d99a6637f5a1c5
ekjot2345-collab <ekjot2345@gmail.com>
2026-04-16 19:00:08 +0530

    ch

--- test/pattern_1.cpp
+++ test/pattern_1.cpp
@@ -23,6 +23,7 @@ int main(){
     }
     cout<<endl;
     i=i+1;
+    }
 
    
 
@@ -43,6 +44,7 @@ int n;
     }
     cout<<endl;
     i=i+1;
+    }
 
 
 // 111
@@ -65,7 +67,7 @@ int n;
     }
     cout<<endl;
     i=i+1;
-
+}
 
 // 123
 // 123
@@ -73,47 +75,75 @@ int n;
 
 */
 
+//counting
+ /*int n;
+ cin>>n;
+
+ int i=1;
+ int count=1;
+ while(i<=n){
+
+    int j=1;
+    while(j<=n){
+        cout<<count<<" ";
+        count=count+1;
+        j=j+1;
+
+    }
+    cout<<endl;
+    i=i+1;
+    }
+    */
+// 1 2 3 
+// 4 5 6
+// 7 8 9
 
 
 
 
 
+/*
+int n;
+ cin>>n;
+
+ int i=1;
+ while(i<=n){
+
+    int j=1;
+    while(j<=i){
+        cout<<'*'<<" ";
+        j=j+1;
+
+    }
+    cout<<endl;
+    i=i+1;}
+* 
+* *
+* * *
+* * * *
+* * * * * 
+  // 
+   
 
 
 
+int n;
+ cin>>n;
 
+ int i=1;
+ while(i<=n){
 
+    int j=1;
+    while(j<=i){
+        cout<<i<<" ";
+        j=j+1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+    cout<<endl;
+    i=i+1;}
+1 
+2 2
+3 3 3
+4 4 4 4 /// */
 }
-
-
-
-
 
